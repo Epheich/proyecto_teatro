ae2dc9f61c4e9abb50b2429f43b6deeeab16fc8c
Moises <moisesjimenezdeharo1smr@gmail.com>
2022-07-06 16:55:58 +0200

    proyecto_teatro

---
+++ src/menu.ts
@@ -0,0 +1,17 @@
+export function mostrarMenu(): void {
+    console.clear();
+    console.log('TEATRO LA BOMBILLA DE DON BLAS');
+    console.log('------------------------');
+    console.log("1. Ver la programación actual");
+    console.log("2. Mostrar todas las localidades");
+    console.log("3. Mostrar localidades ocupadas");
+    console.log("4. Vender localidad");
+    console.log("5. Cancelar localidad");
+    console.log("6. Consultar localidad");
+    console.log("7. Calcular recaudacion");
+    console.log("8. Terminar programa");
+}
+
+export function gestionarOpcion(opcion: string, rlp): void {
+    
+}
